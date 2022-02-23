c4496ee18b4ed1c317dc5c5f53d1e6170ce1f24b
aperkes <perkes.ammon@gmail.com>
2022-02-23 12:55:22 -0800

    updated for .zip uploading

---
+++ recording/src/sync_zips.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+## The start of this replicates a lot of  run_cam.sh to get the correct file names
+## It then combines all the images into a .zip and drops it in recording
+
+## Find the name, regardless of the pi.
+pi_name=${HOSTNAME: -4}
+if [[ $pi_name == "rypi" ]]; then
+    pi_name='pi01'
+fi
+
+## Copy only the *.zip files in the recording directory (without checking sub directories)
+rclone copy /home/pi/recording --include "*.zip" --max-depth 1 AmazonBox:/pivideos/$pi_name
---
+++ recording/src/zip_today.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+## The start of this replicates a lot of  run_cam.sh to get the correct file names
+## It then combines all the images into a .zip and drops it in recording
+
+## Find the name, regardless of the pi.
+pi_name=${HOSTNAME: -4}
+if [[ $pi_name == "rypi" ]]; then
+    pi_name='pi01'
+fi
+
+year_stamp=$(date "+%Y.%m")
+date_stamp=$(date "+%Y.%m.%d")
+
+## Use the batch.trex. tag if you want to have this batch processed
+directory_path=/home/pi/recording/batch.trex.$date_stamp/ 
+
+#directory_path=/home/pi/recording/$date_stamp/ 
+
+# Again, use the batch.trex. tag if you want this to be grabbed during batch processing
+zip -r /home/pi/recording/batch.trex.$date_stamp.zip $directory_path
