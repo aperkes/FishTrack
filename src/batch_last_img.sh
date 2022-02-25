b09a55de92211d8275077e7ea6b14bb7d4e17e7f
aperkes <perkes.ammon@gmail.com>
2022-02-25 12:06:20 -0800

    script to grab last image

---
+++ src/batch_last_img.sh
@@ -0,0 +1,13 @@
+
+date_stamp=$(date "+%Y.%m.%d")
+
+remote_dir="/home/pi/recording/$date_stamp.batch.trex"
+local_dir="/home/ammon/Documents/Scripts/FishTrack/recording/now"
+
+## Remove old images
+rm $local_dir/*.jpg
+
+for i in $(cat $1); do
+    scp $i:$remote_dir/$(ssh $i "ls $remote_dir | tail -2 | head -1") $local_dir
+
+done
