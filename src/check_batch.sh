25d25ac246868858c78d860c40833e4de9b0dd34
aperkes <perkes.ammon@gmail.com>
2022-02-28 12:26:23 -0800

    small debug

--- src/check_batch.sh
+++ src/check_batch.sh
@@ -6,11 +6,14 @@ secs0=$(date -d "1970-01-01 $current_time" +%s)
 
 ## Calculate time in seconds that each image was saved
 n_files=$(ls /home/ammon/Documents/Scripts/FishTrack/recording/now | wc -l)
-n_servers=$(cat /home/ammon/Documents/Scripts/FishTrack/$1 | wc -l)
+n_servers=$(cat $1 | wc -l)
 
+echo $current_time
 if [ "$n_files" -lt "$n_servers" ]; then
-    #echo 'Missing files'
+    echo 'Missing files!'
     bash /home/ammon/Documents/Scripts/FishTrack/src/send_mail.sh "ALERT: Images missing in last upload"
+else
+    echo "Images checked, all present"
 fi
 
 for i in $(ls /home/ammon/Documents/Scripts/FishTrack/recording/now); do
@@ -20,12 +23,15 @@ for i in $(ls /home/ammon/Documents/Scripts/FishTrack/recording/now); do
     sec=${ds:4:2}
     secs=$(date -d "1970-01-01 $hour:$min:$sec" +%s)
 
-    echo "Difference:"
     diff="$(($secs - $secs0))"
     diff=${diff#-}
     if [ "$diff" -gt "1200" ]; then
-        #echo 'too old'
+        echo "ALERT: $i too old"
         bash /home/ammon/Documents/Scripts/FishTrack/src/send_mail.sh "ALERT: $i is too old, check it!"
+    else
+        echo "$i - ok"
     fi
-    break
 done
+echo 'Times checked, all done. Have a nice day'
+
+bash /home/ammon/Documents/Scripts/FishTrack/src/send_mail.sh "Test: Everything appears to be working"
