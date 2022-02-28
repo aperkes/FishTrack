090dd75e0291de46ff7f8e05f141ed65dde47cdc
aperkes <perkes.ammon@gmail.com>
2022-02-28 14:57:56 -0800

    tidying up

--- src/template_mail.sh
+++ src/template_mail.sh
@@ -3,5 +3,7 @@
 ## Very simple script to run a message (defined by quotes, e.g. bash ./send_mail.sh 'this is a test'
 ## The URL there had to be configured via the app, talk to Ammon for questions if it's not working
 
+# I can't post the real url to github, so this script is actually a mockup, change the name before use
+
 message='{"text":"'$1'"}'
-curl -X POST -H 'Content-type: application/json' --data "$message" <<YOUR WEBHOOK HERE>>
+curl -X POST -H 'Content-type: application/json' --data "$message" YOUR.WEBHOOK.HERE
